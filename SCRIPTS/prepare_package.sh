832c9d5bc5ff0d7e8af2ed0f724bad52375c5203
SiYao Mo <1388852+msylgj@users.noreply.github.com>
2021-08-16 08:47:40 +0800

    fix error

--- SCRIPTS/prepare_package.sh
+++ SCRIPTS/prepare_package.sh
@@ -17,7 +17,7 @@ patch -p1 < ../PATCHES/001-fix-firewall-flock.patch
 ### 获取额外的 LuCI 应用、主题和依赖 ###
 # lucihttp
 rm -rf feeds/luci/contrib/package/lucihttp
-svn co https://github.com/immortalwrt/luci/trunk/contrib/package/lucihttp feeds/luci/contrib/package/lucihttp
+svn co https://github.com/immortalwrt/luci/branches/master/contrib/package/lucihttp feeds/luci/contrib/package/lucihttp
 # MOD Argon
 pushd feeds/luci/themes/luci-theme-argonv3
 wget -qO- https://github.com/msylgj/luci-theme-argon/commit/0710026.patch | patch -p1
