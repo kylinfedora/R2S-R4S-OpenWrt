6a146c37939d43de316bd4f1c135abe4fbe33111
SiYao Mo <1388852+msylgj@users.noreply.github.com>
2021-08-16 08:47:07 +0800

    fix error

--- SCRIPTS/prepare_package.sh
+++ SCRIPTS/prepare_package.sh
@@ -15,6 +15,9 @@ sed -i 's/Os/O3 -funsafe-math-optimizations -funroll-loops -ffunction-sections -
 patch -p1 < ../PATCHES/001-fix-firewall-flock.patch
 
 ### 获取额外的 LuCI 应用、主题和依赖 ###
+# lucihttp
+rm -rf feeds/luci/contrib/package/lucihttp
+svn co https://github.com/immortalwrt/luci/trunk/contrib/package/lucihttp feeds/luci/contrib/package/lucihttp
 # MOD Argon
 pushd feeds/luci/themes/luci-theme-argonv3
 wget -qO- https://github.com/msylgj/luci-theme-argon/commit/0710026.patch | patch -p1
@@ -39,6 +42,7 @@ sed -i "s/'%D %V %C'/'Built by OPoA($(date +%Y.%m.%d))@%D %V'/g" package/base-fi
 sed -i "/DISTRIB_REVISION/d" package/base-files/files/etc/openwrt_release
 sed -i "/%D/a\ Built by OPoA($(date +%Y.%m.%d))" package/base-files/files/etc/banner
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/+luci-lib-ipkg //g' package/emortal/addition-trans-zh/Makefile
 sed -i "s/openclash.config.enable=0/openclash.config.enable=1/g" feeds/luci/applications/luci-app-openclash/root/etc/uci-defaults/luci-openclash
 sed -i 's/1608/1800/g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/cpufreq
 sed -i 's/2016/2208/g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/cpufreq
