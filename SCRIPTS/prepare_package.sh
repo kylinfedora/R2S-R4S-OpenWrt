7c3e305d68dafa46e672a04079a5adfc9c4b9027
SiYao Mo <1388852+msylgj@users.noreply.github.com>
2021-08-17 12:44:43 +0800

    udpate

--- SCRIPTS/prepare_package.sh
+++ SCRIPTS/prepare_package.sh
@@ -31,6 +31,8 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash feeds/luc
 svn co https://github.com/QiuSimons/OpenWrt-Add/trunk/addition-trans-zh package/emortal/addition-trans-zh
 cp -f ../SCRIPTS/zzz-default-settings package/emortal/addition-trans-zh/files/zzz-default-settings
 rm -rf package/emortal/addition-trans-zh/status
+sed -i '/status/d' package/emortal/addition-trans-zh/Makefile
+sed -i 's/+luci-lib-ipkg //g' package/emortal/addition-trans-zh/Makefile
 
 ### 最后的收尾工作 ###
 # Lets Fuck
@@ -41,7 +43,6 @@ sed -i "s/'%D %V %C'/'Built by OPoA($(date +%Y.%m.%d))@%D %V'/g" package/base-fi
 sed -i "/DISTRIB_REVISION/d" package/base-files/files/etc/openwrt_release
 sed -i "/%D/a\ Built by OPoA($(date +%Y.%m.%d))" package/base-files/files/etc/banner
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i 's/+luci-lib-ipkg //g' package/emortal/addition-trans-zh/Makefile
 sed -i "s/openclash.config.enable=0/openclash.config.enable=1/g" feeds/luci/applications/luci-app-openclash/root/etc/uci-defaults/luci-openclash
 sed -i 's/1608/1800/g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/cpufreq
 sed -i 's/2016/2208/g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/cpufreq
